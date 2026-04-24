a560e86dd2555921a89f7247f4f3dade8506bfa9
elliott <elliott.cn.lee@gmail.com>
2026-04-24 18:02:13 +1000

    commit to support jupyterlab

--- web_intf/vite.config.js
+++ web_intf/vite.config.js
@@ -1,11 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  // VERY IMPORTANT
+  base: './',
+
   plugins: [react()],
+
   server: {
-    host: 'localhost',
+    // MUST use 0.0.0.0
+    host: '0.0.0.0',
+
     port: 5173,
-  }
+
+    strictPort: true,
+
+    // IMPORTANT for SageMaker/Jupyter proxy
+    hmr: false,
+
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+
+      '/health': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
